4b7563f485640f8f36f40ee168842f9fe9a2abc9
Ralph Slooten <axllent@gmail.com>
2019-08-27 14:49:44 +1200

    Remove `Archive` button from gridfield

--- src/MetaEditor.php
+++ src/MetaEditor.php
@@ -1,79 +1,125 @@
 <?php
-
 namespace Axllent\MetaEditor;
 
+use Axllent\MetaEditor\Forms\GridField\GridFieldLevelup;
+use Axllent\MetaEditor\Forms\MetaEditorDescriptionColumn;
 use Axllent\MetaEditor\Forms\MetaEditorPageColumn;
 use Axllent\MetaEditor\Forms\MetaEditorPageLinkColumn;
 use Axllent\MetaEditor\Forms\MetaEditorTitleColumn;
-use Axllent\MetaEditor\Forms\MetaEditorDescriptionColumn;
 use SilverStripe\Admin\ModelAdmin;
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Core\ClassInfo;
-use Axllent\MetaEditor\Forms\GridField\GridFieldLevelup;
-use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\GridField\GridFieldEditButton;
+use SilverStripe\Forms\GridField\GridFieldExportButton;
+use SilverStripe\Forms\GridField\GridFieldImportButton;
+use SilverStripe\Forms\GridField\GridFieldPrintButton;
+use SilverStripe\Forms\GridField\GridField_ActionMenu;
+use SilverStripe\Forms\GridField\GridField_ActionMenuItem;
 use SilverStripe\View\Requirements;
-use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\CheckboxField;
-use SilverStripe\Forms\TextField;
-use SilverStripe\ORM\Filters\PartialMatchFilter;
 
 class MetaEditor extends ModelAdmin
 {
 
     /**
+     * Meta title field
+     *
      * @config string
      */
     private static $meta_title_field = 'Title';
+
+    /**
+     * Meta title minimum length
+     *
+     * @var int
+     */
     private static $meta_title_min_length = 20;
+
+    /**
+     * Meta title maximum length
+     *
+     * @var int
+     */
     private static $meta_title_max_length = 70;
 
     /**
+     * Meta description field
+     *
      * @config string
      */
     private static $meta_description_field = 'MetaDescription';
+
+    /**
+     * Meta description field minimum length
+     *
+     * @var int
+     */
     private static $meta_description_min_length = 100;
+
+    /**
+     * Meta description field maximum length
+     *
+     * @var int
+     */
     private static $meta_description_max_length = 200;
 
     /**
-     * Non-editable pages (includes extended classes)
+     * Non-editable pages (includes all classes extending these)
+     *
      * @config array
      */
     private static $non_editable_page_types = [
         'SilverStripe\\CMS\\Model\\RedirectorPage',
-        'SilverStripe\\CMS\\Model\\VirtualPage'
+        'SilverStripe\\CMS\\Model\\VirtualPage',
     ];
 
     /**
-     * Hidden pages (includes extended classes)
+     * Hidden pages (includes all classes extending these)
      * Note that these (or their children) are not displayed
+     *
      * @config array
      */
     private static $hidden_page_types = [
-     'SilverStripe\\ErrorPage\\ErrorPage'
+        'SilverStripe\\ErrorPage\\ErrorPage',
     ];
 
     /**
+     * CMS menu title
+     *
      * @var string
      */
     private static $menu_title = 'Meta Editor';
 
     /**
+     * CMS url segment
+     *
      * @var string
      */
     private static $url_segment = 'meta-editor';
 
     /**
+     * CMS menu icon
+     *
      * @var string
      */
     private static $menu_icon = 'axllent/silverstripe-meta-editor: images/MetaEditor.svg';
 
     /**
+     * CMS managed modals
+     *
      * @var array
      */
     private static $managed_models = [
-        'SilverStripe\\CMS\\Model\\SiteTree'
+        SiteTree::class,
     ];
 
+    /**
+     * Init
+     *
+     * @return void
+     */
     public function init()
     {
         parent::init();
@@ -81,6 +127,14 @@ class MetaEditor extends ModelAdmin
         Requirements::javascript('axllent/silverstripe-meta-editor: javascript/meta-editor.js');
     }
 
+    /**
+     * Get edit form
+     *
+     * @param int   $id     ID
+     * @param array $fields Array
+     *
+     * @return mixed
+     */
     public function getEditForm($id = null, $fields = null)
     {
         $form = parent::getEditForm($id, $fields);
@@ -90,14 +144,16 @@ class MetaEditor extends ModelAdmin
         $grid = $form->Fields()->dataFieldByName($gridFieldName);
         if ($grid) {
             $config = $grid->getConfig();
-            $config->removeComponentsByType('SilverStripe\\Forms\\GridField\\GridFieldAddNewButton');
-            $config->removeComponentsByType('SilverStripe\\Forms\\GridField\\GridFieldPrintButton');
-            $config->removeComponentsByType('SilverStripe\\Forms\\GridField\\GridFieldEditButton');
-            $config->removeComponentsByType('SilverStripe\\Forms\\GridField\\GridFieldExportButton');
-            $config->removeComponentsByType('SilverStripe\\Forms\\GridField\\GridFieldImportButton');
-            $config->removeComponentsByType('SilverStripe\\Forms\\GridField\\GridFieldDeleteAction');
+            $config->removeComponentsByType(GridFieldAddNewButton::class);
+            $config->removeComponentsByType(GridFieldPrintButton::class);
+            $config->removeComponentsByType(GridFieldEditButton::class);
+            $config->removeComponentsByType(GridFieldExportButton::class);
+            $config->removeComponentsByType(GridFieldImportButton::class);
+            $config->removeComponentsByType(GridFieldDeleteAction::class);
+            $config->removeComponentsByType(GridField_ActionMenu::class);
+            $config->removeComponentsByType(GridField_ActionMenuItem::class);
 
-            $parent_id = $this->request->requestVar('ParentID') ? : 0;
+            $parent_id = $this->request->requestVar('ParentID') ?: 0;
 
             if ($parent_id) {
                 $parent = SiteTree::get()->byID($parent_id);
@@ -124,14 +180,15 @@ class MetaEditor extends ModelAdmin
                 $config->addComponent($uplink);
             }
 
-            $config->getComponentByType('SilverStripe\\Forms\\GridField\GridFieldDataColumns')->setDisplayFields(
-                [
-                    'MetaEditorPageColumn' => 'Page',
-                    'MetaEditorTitleColumn' => 'Meta Title',
-                    'MetaEditorDescriptionColumn' => 'Meta Description',
-                    'MetaEditorPageLinkColumn' => ''
-                ]
-            );
+            $config->getComponentByType(GridFieldDataColumns::class)
+                ->setDisplayFields(
+                    [
+                        'MetaEditorPageColumn'        => 'Page',
+                        'MetaEditorTitleColumn'       => 'Meta Title',
+                        'MetaEditorDescriptionColumn' => 'Meta Description',
+                        'MetaEditorPageLinkColumn'    => '',
+                    ]
+                );
 
             $config->addComponent(new MetaEditorPageColumn());
             $config->addComponent(new MetaEditorTitleColumn());
@@ -140,20 +197,6 @@ class MetaEditor extends ModelAdmin
         }
 
         return $form;
-    }
-
-    public function getSearchContext()
-    {
-        $context = parent::getSearchContext();
-
-        $fields = FieldList::create(
-            TextField::create('Search', 'Search page, meta title & description'),
-            CheckboxField::create('PagesWithWarnings', 'Pages with warnings')
-        );
-
-        $context->setFields($fields);
-
-        return $context;
     }
 
     /**
@@ -165,45 +208,53 @@ class MetaEditor extends ModelAdmin
     {
         $list = parent::getList();
 
-        $parent_id = $this->request->requestVar('ParentID') ? : 0;
+        $parent_id = $this->request->requestVar('ParentID') ?: 0;
 
         $search_filter = false;
 
+        $conf = $this->config();
+
         if (!empty($this->request->requestVar('Search'))) {
-            $search = $this->request->requestVar('Search');
+            $search        = $this->request->requestVar('Search');
             $search_filter = true;
-            $list = $list->filterAny([
-                'MenuTitle' . ':PartialMatch' => $search,
-                $this->config()->meta_title_field . ':PartialMatch' => $search,
-                $this->config()->meta_description_field . ':PartialMatch' => $search
-            ]);
+            $list          = $list->filterAny(
+                [
+                    'MenuTitle' . ':PartialMatch'                   => $search,
+                    $conf->meta_title_field . ':PartialMatch'       => $search,
+                    $conf->meta_description_field . ':PartialMatch' => $search,
+                ]
+            );
         }
         if (!empty($this->request->requestVar('EmptyMetaDescriptions'))) {
             $search_filter = true;
-            $list = $list->where($this->config()->meta_description_field . ' IS NULL');
+            $list          = $list->where(
+                $this->config()->meta_description_field . ' IS NULL'
+            );
         }
         if (!empty($this->request->requestVar('PagesWithWarnings'))) {
             $search_filter = true;
-            $list = $list->filterByCallback(function ($item) {
-                if (
-                    !empty(MetaEditorTitleColumn::getErrors($item)) ||
-                    !empty(MetaEditorDescriptionColumn::getErrors($item))
-                ) {
-                    return true;
-                }
-                return false;
-            });
-        }
+            $list          = $list->filterByCallback(
+                function ($item) {
+                    if (!empty(MetaEditorTitleColumn::getErrors($item))
+                        || !empty(MetaEditorDescriptionColumn::getErrors($item))
+                    ) {
+                        return true;
+                    }
 
+                    return false;
+                }
+            );
+        }
 
         if ($this->config()->hidden_page_types) {
             $ignore = [];
             foreach ($this->config()->hidden_page_types as $class) {
                 $subclasses = ClassInfo::getValidSubClasses($class);
-                $ignore = array_merge(array_keys($subclasses), $ignore);
+                $ignore     = array_merge(array_keys($subclasses), $ignore);
             }
             if (!empty($ignore)) {
-                $list = $list->exclude('ClassName', $ignore); // remove error pages etc
+                // remove error pages etc
+                $list = $list->exclude('ClassName', $ignore);
             }
         }
 
